05eb6ac33feb585e500e8d6d7b517a9aab9a5161
Jorge Aranda <jorge.aranda@cuevano.ca>
2022-02-03 21:15:17 -0800

    Add an ugly but serviceable loader from file

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import filecmp
+
 from cards import Cards
 
 
@@ -17,19 +19,95 @@ class Game:
         self.cards.setup()
         return self
 
+    def from_file(self, path):
+        self.cards = Cards()
+        with open(path) as gamefile:
+            lines = [line.strip() for line in gamefile.readlines()]
+
+        for idx, line in enumerate(lines):
+            match line:
+                case "# Meta":
+                    self.load_meta(idx + 1, lines)
+                case "# General":
+                    self.load_state(idx + 1, lines)
+                case "# Cards":
+                    self.load_cards(idx + 1, lines)
+        return self
+
+    def load_meta(self, idx, lines):
+        for line in lines[idx:]:
+            if line.startswith("---"):
+                break
+            label, value = line.split(": ", 1)
+            match label:
+                case "Name":
+                    self.name = value
+                case "Created":
+                    self.created = value
+                case "USSR":
+                    self.ussr_name = value
+                case "USA":
+                    self.usa_name = value
+                case "Bid":
+                    self.bid = value
+
+    def load_state(self, idx, lines):
+        for line in lines[idx:]:
+            if line.startswith("---"):
+                break
+            label, value = line.split(": ", 1)
+            match label:
+                case "Status":
+                    self.status = value
+                case "VPs":
+                    self.score = value
+                case "DEFCON":
+                    self.defcon = value
+                case "USSR Space Race":
+                    self.ussr_space_race = value
+                case "USA Space Race":
+                    self.usa_space_race = value
+
+    def load_cards(self, idx, lines):
+        location = None
+        for line in lines[idx:]:
+            if line.startswith("---"):
+                break
+            if len(line) == 0:
+                continue
+            if line == "## USSR":
+                location = "ussr"
+            elif line == "## USA":
+                location = "usa"
+            elif line == "## Board":
+                location = "board"
+            elif line == "## Deck":
+                location = "deck"
+            elif line == "## Discard":
+                location = "discard"
+            elif line == "## Removed":
+                location = "removed"
+            elif line == "## Box":
+                location = "box"
+            else:
+                short = line[:10].strip()
+                self.cards.move(short, location)
+
     def to_file(self, path):
         with open(path, "w") as gamefile:
+            gamefile.write("# Meta\n")
             gamefile.write(f"Name: {self.name}\n")
             gamefile.write(f"Created: {self.created}\n")
             gamefile.write(f"USSR: {self.ussr_name}\n")
             gamefile.write(f"USA: {self.usa_name}\n")
             gamefile.write(f"Bid: {self.bid}\n")
             gamefile.write("-" * 80 + "\n")
+            gamefile.write("# General\n")
             gamefile.write(f"Status: {self.status}\n")
             gamefile.write(f"VPs: {self.score}\n")
             gamefile.write(f"DEFCON: {self.defcon}\n")
             gamefile.write(f"USSR Space Race: {self.ussr_space_race}\n")
-            gamefile.write(f"USA  Space Race: {self.usa_space_race}\n")
+            gamefile.write(f"USA Space Race: {self.usa_space_race}\n")
             gamefile.write("-" * 80 + "\n")
             gamefile.write("# Cards\n")
             gamefile.write("\n## USSR\n")
@@ -53,3 +131,16 @@ class Game:
             gamefile.write("\n## Box\n")
             for card in self.cards.cards_in("box").values():
                 gamefile.write(f"{card.short:10} - {card.ops} {card.full}\n")
+
+
+# -- Tests --
+def test_no_changes():
+    name = "game_test"
+    game = Game().new(name, "a timestamp", "me", "bot", 2)
+    path = f"test_output/{name}.tws"
+    game.to_file(path)
+    game.from_file(path)
+    path2 = f"test_output/{name}_2.tws"
+    game.to_file(path2)
+    assert game.name == name
+    assert filecmp.cmp(path, path2)
